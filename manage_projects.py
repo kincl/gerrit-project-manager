47ac10f5ec1f7c70481a2a11e07e330730188435
Jason Kincl <kincljc@ornl.gov>
2014-10-22 14:11:12 -0400

    Fixing top of script doc

--- manage_projects.py
+++ manage_projects.py
@@ -17,28 +17,28 @@
 
 # manage_projects.py reads a config file called projects.ini
 # It should look like:
-
 # [projects]
-# homepage=http://openstack.org
 # gerrit-host=review.openstack.org
 # local-git-dir=/var/lib/git
 # gerrit-key=/home/gerrit2/review_site/etc/ssh_host_rsa_key
 # gerrit-committer=Project Creator <openstack-infra@lists.openstack.org>
 # acl-dir=/home/gerrit2/acls
 # acl-base=/home/gerrit2/acls/project.config
+# cache-dir=/tmp/cache
+# gerrit-user=gerrit2
+# gerrit-system-user=gerrit2
+# gerrit-system-group=gerrit2
 #
 # manage_projects.py reads a project listing file called projects.yaml
 # It should look like:
 # - project: PROJECT_NAME
 #   options:
 #    - track-upstream
-#   homepage: Some homepage that isn't http://openstack.org
+#    - no-gerrit
 #   description: This is a great project
 #   upstream: https://gerrit.googlesource.com/gerrit
 #   upstream-prefix: upstream
-#   acl-config: /path/to/gerrit/project.config
-#   acl-append:
-#     - /path/to/gerrit/project.config
+#   acl-config: project.config
 #   acl-parameters:
 #     project: OTHER_PROJECT_NAME
 
