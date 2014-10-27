23dfe4df103b50e62b7cbf3c32697807ccc76acb
Jason Kincl <kincljc@ornl.gov>
2014-10-27 09:40:05 -0400

    removing debug statements

--- gerrit_projects/projects.py
+++ gerrit_projects/projects.py
@@ -638,13 +638,11 @@ def main():
                 if project['track_upstream']:
                     sync_upstream(repo_path, project, ssh_env)
 
-                if project['acl_config'] and os.path.exists(os.path.join(ACL_DIR, project['acl_config'])):
-                    print 'yes'
+                if project['acl_config'] and os.path.isfile(os.path.join(ACL_DIR, project['acl_config'])):
                     process_acls(
                         project, ACL_DIR, remote_url, repo_path,
                         ssh_env, gerrit, GERRIT_GITID)
                 else:
-                    print 'no'
                     if project['description']:
                         gerrit.updateProject(project['name'], 'description', project['description'])
 
